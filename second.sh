f4706b1c488b3e502396a0cfceec07155409b768
NatarajVeerapur <natarajv.mca22@rvce.edu.in>
2023-03-01 10:35:03 +0530

    lss

---
+++ second.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+VAR = 11111
+TEST= RVCE
+#DOUBLE QUOTES
+echo " double quote $VAR $TEST"
+#SINGLE QUOTE
+echo 'single quote $VAR $TEST'
+#REVERSE QUOTE / back tick
+echo `host name`
+
+
